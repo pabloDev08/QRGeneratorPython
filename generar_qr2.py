79e6da53abb96330309efc13729b53de1e00c192
pabloDev08 <pablo.igealuengo@gmail.com>
2025-03-27 16:57:15 +0100

    Add files via upload

---
+++ generar_qr2.py
@@ -0,0 +1,24 @@
+import qrcode 
+from PIL import Image 
+
+def generate_qr_code(text,file_name):
+    qr = qrcode.QRCode(
+        version = 1,
+        error_correction = qrcode.constants.ERROR_CORRECT_H,
+        box_size = 20,
+        border = 4,       
+    )
+
+    qr.add_data(text)
+    qr.make(fit = True)
+
+    img = qr.make_image(fill_color = "#000000", back_color = "#FFFFFF")
+
+    img.save(file_name)
+    print(f"QR code guardado como {file_name}")
+
+if __name__ == "__main__":
+    text = input("Introduce un texto o una URL para generar el código QR")
+    file_name = input("Introduce el nombre del código QR que contenga SIEMPRE la extensión .png")
+    generate_qr_code(text,file_name)
+
